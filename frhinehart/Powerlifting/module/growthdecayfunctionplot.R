f6d0647f7eba14c570c3bb3fafef1642d7e3cc42
iramler <iramler@users.noreply.github.com>
2025-06-10 12:48:43 -0400

    curve plots

--- frhinehart/Powerlifting/module/growthdecayfunctionplot.R
+++ frhinehart/Powerlifting/module/growthdecayfunctionplot.R
@@ -61,3 +61,45 @@ dat = cbind(x=datx,y=daty,a=data)
 
 ggplot(dat,aes(x=x,y=y,color=as.factor(a))) + 
   geom_smooth(method="loess",aes(color=as.factor(a)),se=FALSE) 
+
+
+
+################################
+# tidying up
+
+library(dplyr)
+
+growth_decay_curve1 <- function(x, a, b) {
+  a * x * exp(-b * x)
+}
+
+# Define inputs
+x_vals <- seq(0, 300, by = 0.1)
+a_vals <- c(5, 10, 20, 25)
+b_vals <- c(0.005, 0.01, 0.025, 0.04)
+
+# Create the combined data frame
+combined_df <- expand.grid(x = x_vals, a = a_vals, b = b_vals) %>%
+  mutate(y = growth_decay_curve1(x, a, b),
+         a = as.factor(a),
+         b = as.factor(b))
+
+# Preview the result
+head(combined_df)
+
+
+ggplot(combined_df, aes(x = x, y = y, color = a)) + 
+  geom_smooth(method = "loess", se = FALSE) +
+  facet_wrap(~ b, scales = "free_y", 
+             labeller = purrr::partial(label_both, sep = " = ")) +
+  theme_bw() +
+  theme(legend.position = "bottom") +
+  theme(
+    legend.position = "bottom",
+    strip.text = element_text(face = "bold")  # Make facet labels bold
+  ) +
+  labs(color = "a")
+
+
+
+
